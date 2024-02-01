f1d663c4047749a77cc3328fb35b9442756e4c77
basma ebraheim <basmaebraheim@gmail.com>
2024-02-02 01:36:38 +0200

    update

--- find_function.c
+++ find_function.c
@@ -11,33 +11,33 @@
  */
 void get_function(char *opcode, char *value, int ln, int format)
 {
-	int i;
-	int flag;
+int i;
+int flag;
 
-	instruction_t func_list[] = {
-		{"nop", nop},
-		{"push", push_stack},
-		{"pall", pall_stack},
-        	{"pint", print_top},
-		{"pop", pop_stack},
-		{"add", add_toptwo_nodes},
-		{"swap", swap},
-		{NULL, NULL}
-	};
+instruction_t func_list[] = {
+{"nop", nop},
+{"push", push_stack},
+{"pall", pall_stack},
+{"pint", print_top},
+{"pop", pop_stack},
+{"add", add_toptwo_nodes},
+{"swap", swap},
+{NULL, NULL}
+};
 
-	if (opcode[0] == '#')
-		return;
+if (opcode[0] == '#')
+return;
 
-	for (flag = 1, i = 0; func_list[i].opcode != NULL; i++)
-	{
-		if (strcmp(opcode, func_list[i].opcode) == 0)
-		{
-			function_handler(func_list[i].f, opcode, value, ln, format);
-			flag = 0;
-		}
-	}
-	if (flag == 1)
-		print_error(3, ln);
+for (flag = 1, i = 0; func_list[i].opcode != NULL; i++)
+{
+if (strcmp(opcode, func_list[i].opcode) == 0)
+{
+function_handler(func_list[i].f, opcode, value, ln, format);
+flag = 0;
+}
+}
+if (flag == 1)
+print_error(3, ln);
 }
 
 /**
@@ -47,10 +47,10 @@ void get_function(char *opcode, char *value, int ln, int format)
  */
 void push_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
-	stack_t *tmp;
+stack_t *tmp;
 
-	if (!new_node || !*new_node)
-		exit(EXIT_FAILURE);
+if (!new_node || !*new_node)
+exit(EXIT_FAILURE);
 if (head)
 {
 tmp = head;
@@ -68,17 +68,17 @@ head = *new_node;
  */
 void pall_stack(stack_t **top_node, unsigned int ln)
 {
-	stack_t *tmp;
+stack_t *tmp;
 
-	(void) ln;
-	if (top_node == NULL)
-		exit(EXIT_FAILURE);
-	tmp = *top_node;
-	while (tmp != NULL)
-	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-	}
+(void) ln;
+if (top_node == NULL)
+exit(EXIT_FAILURE);
+tmp = *top_node;
+while (tmp != NULL)
+{
+printf("%d\n", tmp->n);
+tmp = tmp->next;
+}
 }
 /**
  * print_top - Print top node of the stack.
@@ -87,9 +87,9 @@ void pall_stack(stack_t **top_node, unsigned int ln)
  */
 void print_top(stack_t **head, unsigned int ln)
 {
-	if (!head || !*head)
-		print_error2(6, ln);
-	printf("%d\n", (*head)->n);
+if (!head || !*head)
+print_error2(6, ln);
+printf("%d\n", (*head)->n);
 }
 
 /**
@@ -103,33 +103,33 @@ void print_top(stack_t **head, unsigned int ln)
  */
 void function_handler(op_func func, char *op, char *val, int ln, int format)
 {
-	stack_t *node;
-	int flag;
-	int i;
+stack_t *node;
+int flag;
+int i;
 
-	flag = 1;
-	if (strcmp(op, "push") == 0)
-	{
-		if (val && val[0] == '-')
-		{
-			val = val + 1;
-			flag = -1;
-		}
-		if (!val)
-			print_error(5, ln);
-		for (i = 0; val[i] != '\0'; i++)
-		{
-			if (isdigit(val[i]) == 0)
-				print_error(5, ln);
-		}
-		node = generate_node(atoi(val) * flag);
-		if (format == 0)
-			func(&node, ln);
-		if (format == 1)
-			push_queue(&node, ln);
-	}
-	else
-		func(&head, ln);
+flag = 1;
+if (strcmp(op, "push") == 0)
+{
+if (val && val[0] == '-')
+{
+val = val + 1;
+flag = -1;
+}
+if (!val)
+print_error(5, ln);
+for (i = 0; val[i] != '\0'; i++)
+{
+if (isdigit(val[i]) == 0)
+print_error(5, ln);
+}
+node = generate_node(atoi(val) * flag);
+if (format == 0)
+func(&node, ln);
+if (format == 1)
+push_queue(&node, ln);
+}
+else
+func(&head, ln);
 }
 /**
  * add_toptwo_nodes - Adds the top two elements of the stack.
@@ -138,13 +138,13 @@ void function_handler(op_func func, char *op, char *val, int ln, int format)
  */
 void add_toptwo_nodes(stack_t **stack, unsigned int ln)
 {
-	if (!stack || !*stack || !(*stack)->next)
-		print_error2(8, ln, "add");
+if (!stack || !*stack || !(*stack)->next)
+print_error2(8, ln, "add");
 
-	(*stack) = (*stack)->next;
-	(*stack)->n = (*stack)->n + (*stack)->prev->n;
-	free((*stack)->prev);
-	(*stack)->prev = NULL;
+(*stack) = (*stack)->next;
+(*stack)->n = (*stack)->n + (*stack)->prev->n;
+free((*stack)->prev);
+(*stack)->prev = NULL;
 }
 /**
  * nop - doesn’t do anything.
@@ -153,6 +153,6 @@ void add_toptwo_nodes(stack_t **stack, unsigned int ln)
  */
 void nop(stack_t **stack, unsigned int ln)
 {
-	(void)stack;
-	(void)ln;
+(void)stack;
+(void)ln;
 }
